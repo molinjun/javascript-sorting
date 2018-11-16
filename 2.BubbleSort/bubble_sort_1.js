124da700cd0d82e6e69294f0b80a5d21123d7944
dennis.ge <gezq@yonyou.com>
2018-11-16 09:50:03 +0800

    feat(images): add images for show algorithms

--- 2.BubbleSort/bubble_sort_1.js
+++ 2.BubbleSort/bubble_sort_1.js
@@ -4,7 +4,9 @@ const bubbleSort = (array) => {
 
     // 迭代次数 数组长度-1
     for (let i = 0; i < originValues.length - 1; i++) {
+        // 两两比较，该迭代的最大数，移动到右侧相应位置
         for (let j = 0; j < originValues.length - 1 - i; j++) {
+            // 如果前一个数，大于后一个数，交换
             if (originValues[j] > originValues[j + 1]) {
                 const tmp = originValues[j];
                 originValues[j] = originValues[j + 1];
@@ -15,9 +17,9 @@ const bubbleSort = (array) => {
 
     return originValues;
 };
-const arr = [5, 3, 6, 9, 1];
+const arr = [6, 5, 3, 1, 8, 7, 2, 4];
 console.time();
 const sortedArr = bubbleSort(arr);
 console.timeEnd();
-console.log(arr);
-console.log(sortedArr);
+console.log('origin array: ', arr);
+console.log('sorted array: ', sortedArr);
